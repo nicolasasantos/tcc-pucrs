c85673f54caef452772485399edbeff8d325eeb0
Nicolas Santos <nicolasasantos@outlook.com>
2025-12-28 02:50:42 -0300

    fix: change backend address to acomodate testing on different devices and not only local host

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://192.168.15.20:3000',
   apiKey: 'AIzaSyBHiZl7yuVlKjRFYG6jG2wqYxpT1aLc33o',
 };
